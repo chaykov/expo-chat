3dcbe8a039274344cf62573bd914ee9543e371f1
Chaykov <toumarenshi@gmail.com>
2024-04-06 12:56:33 +0200

    main

--- context/authContext.js
+++ context/authContext.js
@@ -9,9 +9,9 @@ export const AuthContextProvider = ({ children }) => {
   useEffect(() => {
     // onAuthStateChanged
 
-    setTimeout(() => {
-      setIsAuthenticated(false);
-    }, 3000);
+    // setTimeout(() => {
+    setIsAuthenticated(false);
+    // }, 3000);
   }, []);
 
   const login = async (email, password) => {
